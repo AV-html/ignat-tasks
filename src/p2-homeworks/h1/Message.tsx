4bae47234dd5cb9ea506441ec54781eba802e931
Alexander Volkov <AV-html@yandex.ru>
2022-04-26 22:21:04 +0300

    add component HW1 widthout ready css

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import m from './Message.module.css';
 
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
 
-function Message() {
+function Message(props: MessagePropsType) {
     return (
-        <div>
-
+        <div className={m.message}>
+            <div className={m.avatar}>
+                <img src={props.avatar} alt=" Avatar"/>
+            </div>
+            <div className={m.body}>
+                <div className={m.name}>
+                    {props.name}
+                </div>
+                <div className={m.text}>
+                    {props.message}
+                </div>
+                <div className={m.time}>
+                    {props.time}
+                </div>
+            </div>
         </div>
     )
 }
-
 
 export default Message
