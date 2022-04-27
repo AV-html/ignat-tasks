0611c0abbdb3a662b54533a6e80b26d0e15d8d4f
Alexander Volkov <AV-html@yandex.ru>
2022-04-27 17:53:24 +0300

    add alternative message

--- src/p2-homeworks/h1/AlternativeMessage.tsx
+++ src/p2-homeworks/h1/AlternativeMessage.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import m from "./AlternativeMessage.module.css";
 
-function AlternativeMessage() {
+type AlternativeMessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+
+function AlternativeMessage(props: AlternativeMessagePropsType) {
     return (
-        <div>
-
+        <div className={m.message}>
+            <div className={m.avatar}>
+                <img src={props.avatar} alt=" Avatar"/>
+            </div>
+            <div className={m.body}>
+                <div className={m.name}>
+                    {props.name}
+                </div>
+                <div className={m.text}>
+                    {props.message}
+                    <span/>
+                </div>
+                <div className={m.time}>
+                    {props.time}
+                </div>
+            </div>
         </div>
     )
 }
--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import Message from "./Message";
+import AlternativeMessage from "./AlternativeMessage";
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
     name: 'Some Name',
     message: 'some text',
     time: '22:00',
+}
+
+const alternativeMessageData1 = {
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Gacha Life',
+    message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quod consectetur dolors. Last symbol! ->',
+    time: '21:30',
+}
+const alternativeMessageData2 = {
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Gacha Life',
+    message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Test text!',
+    time: '21:31',
+}
+const alternativeMessageData3 = {
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Gacha Life',
+    message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Goodbye!',
+    time: '21:33',
 }
 
 function HW1() {
@@ -25,7 +45,33 @@ function HW1() {
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeMessage/>*/}
+
+            {/* Изменение border-radius, в зависимости от расположения сообщений: первое, последнее или между.
+            Эксперементировал с редкими псевдоклассами, типа only-child
+
+            Не учтено: ширина поста может быть разная, если текст небольшой
+            */}
+            <div>
+                <AlternativeMessage
+                    avatar={alternativeMessageData1.avatar}
+                    name={alternativeMessageData1.name}
+                    message={alternativeMessageData1.message}
+                    time={alternativeMessageData1.time}
+                />
+                <AlternativeMessage
+                    avatar={alternativeMessageData2.avatar}
+                    name={alternativeMessageData2.name}
+                    message={alternativeMessageData2.message}
+                    time={alternativeMessageData2.time}
+                />
+                <AlternativeMessage
+                    avatar={alternativeMessageData3.avatar}
+                    name={alternativeMessageData3.name}
+                    message={alternativeMessageData3.message}
+                    time={alternativeMessageData3.time}
+                />
+            </div>
+
             <hr/>
         </div>
     )
