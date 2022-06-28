9026e436d02b51e97c96f68524daf8921697c7d0
Alexander Volkov <AV-html@yandex.ru>
2022-06-28 09:56:44 +0300

    Add(HW8): sort & filter state

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -2,9 +2,13 @@ import React, {useState} from 'react'
 import {homeWorkReducer} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
-// export type UserType =
+export type UserType = {
+    _id: number,
+    name: string,
+    age: number
+}
 
-const initialPeople = [
+const initialPeople: Array<UserType> = [
     {_id: 0, name: 'Кот', age: 3},
     {_id: 1, name: 'Александр', age: 66},
     {_id: 2, name: 'Коля', age: 16},
@@ -13,17 +17,36 @@ const initialPeople = [
     {_id: 5, name: 'Ирина', age: 55},
 ]
 
+type FilterType = 'all' | 'sort-up' | 'sort-down' | 'check18+';
+
 function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
+    const [people, setPeople] = useState<Array<UserType>>(initialPeople)
+    const [filter, setFilter] = useState<FilterType>('all')
 
     // need to fix any
-    const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
-        </div>
+    const finalPeople = people.map((p) => (
+        <li key={p._id}>
+            <span>{p.name + ' ' + p.age}</span>
+        </li>
     ))
 
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+    const sortUp = () => {
+        setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+        setFilter('sort-up')
+    }
+    const sortDown = () => {
+        setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'}))
+        setFilter('sort-down')
+    }
+    const check = () => {
+        setPeople(homeWorkReducer(initialPeople, {type: 'check', payload: 18}))
+        setFilter('check18+')
+    }
+
+    const styleBackground = {
+        backgroundColor: '#90ee90'
+    }
+
 
     return (
         <div>
@@ -31,11 +54,23 @@ function HW8() {
             homeworks 8
 
             {/*should work (должно работать)*/}
-            {finalPeople}
+            <ul>
+                {finalPeople}
+            </ul>
 
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
+
+            <div>
+                <SuperButton style={filter === 'sort-up' ? styleBackground : {}} onClick={sortUp}>
+                    sort up
+                </SuperButton>
+                <SuperButton style={filter === 'sort-down' ? styleBackground : {}} onClick={sortDown}>
+                    sort down
+                </SuperButton>
+                <SuperButton style={filter === 'check18+' ? styleBackground : {}} onClick={check}>
+                    check 18
+                </SuperButton>
+
+            </div>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
