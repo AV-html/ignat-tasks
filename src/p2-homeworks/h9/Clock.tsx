d3a0b20ce5f06d0378e6b910fb1058c0dc4e08d2
Alexander Volkov <AV-html@yandex.ru>
2022-06-28 22:17:28 +0300

    add(clock): clock & date

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,29 +3,32 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        window.clearInterval(timerId)
     }
     const start = () => {
         stop()
+
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate((date) => {
+                return new Date(date.getTime() + 1000)
+            })
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}` // fix with date
+    const stringDate = `${date.getDay() < 10 ? '0' + date.getDay() : date.getMonth()}:${date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1}:${date.getFullYear()}` // fix with date
 
     return (
         <div>
