268b28eed3873198860930821f785b722b3480c5
Alexander Volkov <AV-html@yandex.ru>
2022-07-09 22:20:51 +0300

    add(HW11): SuperRange component

--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -5,6 +5,7 @@ import HW9 from '../../h9/HW9';
 import HW10 from '../../h10/HW10';
 import {Provider} from 'react-redux';
 import {store} from '../../h10/bll/store';
+import HW11 from '../../h11/HW11';
 
 function PreJunior() {
     return (
@@ -17,7 +18,7 @@ function PreJunior() {
                 <HW10/>
             </Provider>
 
-            {/*<HW11/>*/}
+            <HW11/>
             {/*<HW12/>*/}
         </div>
     )
