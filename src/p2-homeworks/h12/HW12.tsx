2f868cc667e0abdcc81a705923b5a432a0396501
Alexander Volkov <AV-html@yandex.ru>
2022-07-14 16:50:51 +0300

    feat(HW12): changing themes

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import s from "./HW12.module.css";
+import React from 'react';
+import s from './HW12.module.css';
+import SuperSelect from '../h7/common/c5-SuperSelect/SuperSelect';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppStoreType} from '../h10/bll/store';
+import {changeThemeAC, ThemeStateType} from './bll/themeReducer';
 
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
+    const {theme} = useSelector<AppStoreType, ThemeStateType>((state) => state.theme)
+    const dispatch = useDispatch()
 
-    // useDispatch, onChangeCallback
+    function onSelectTheme(theme: string) {
+        dispatch(changeThemeAC(theme))
+    }
 
     return (
         <div className={s[theme]}>
@@ -15,9 +22,13 @@ function HW12() {
                 homeworks 12
             </span>
 
+
+            <div>
+                <SuperSelect value={theme} onChangeOption={onSelectTheme} options={themes}/>
+            </div>
+
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
-
             <hr/>
         </div>
     );
