7dbf77ac0a4e4b5ad1c021b998ae77c88baa6b76
Alexander Volkov <AV-html@yandex.ru>
2022-07-14 16:47:58 +0300

    change Provider from Junior.tsx to index.tsx

--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -3,9 +3,8 @@ import HW7 from '../../h7/HW7';
 import HW8 from '../../h8/HW8';
 import HW9 from '../../h9/HW9';
 import HW10 from '../../h10/HW10';
-import {Provider} from 'react-redux';
-import {store} from '../../h10/bll/store';
 import HW11 from '../../h11/HW11';
+import HW12 from '../../h12/HW12';
 
 function PreJunior() {
     return (
@@ -14,12 +13,9 @@ function PreJunior() {
             <HW7/>
             <HW8/>
             <HW9/>
-            <Provider store={store}>
-                <HW10/>
-            </Provider>
-
+            <HW10/>
             <HW11/>
-            {/*<HW12/>*/}
+            <HW12/>
         </div>
     )
 }
